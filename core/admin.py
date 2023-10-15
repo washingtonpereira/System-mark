017a7d542d0d1968a00534bc68b7b57b5ddb5c83
Washington Pereira <slash.o.teobaldo@gmail.com>
2023-10-15 15:30:54 -0300

    Criados usuarios

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from core.models import Consulta
 
 # Register your models here.
+
+class ConsultaAdmin(admin.ModelAdmin):
+    list_display =('titulo','data_consulta','data_criacao')
+    list_filter = ('usuario',)
+
+
+
+
+
+
+admin.site.register(Consulta,ConsultaAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Consulta(models.Model):
+    titulo = models.CharField(max_length=22)
+    descricao =models.TextField(blank=True, null=True)
+    data_consulta =models.DateField(verbose_name="Data da Consulta")
+    data_criacao = models.DateField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+    
+    def __str__(self):
+        return self.titulo
