eb51fe85b2abb10b7ac0ac0257f5655cc7435b6d
Washington Pereira <slash.o.teobaldo@gmail.com>
2023-10-16 02:51:54 -0300

    Iniciando template

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from core.models import Consulta
+
 
 # Create your views here.
+
+def lista_consultas(request):
+    consulta = Consulta.objects.get(id=1)
+    response = {'consulta':consulta}
+    return render(request, 'agenda.html', response) 
